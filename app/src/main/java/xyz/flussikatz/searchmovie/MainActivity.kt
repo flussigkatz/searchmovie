d1387cf6662edcf1065de6d970c26ad1ddf25a98
flussigkatz <flussigkatz@gmail.com>
2021-04-09 12:54:09 +0300

    Make gradient button background, temp toast for buttons.

--- app/src/main/java/xyz/flussikatz/searchmovie/MainActivity.kt
+++ app/src/main/java/xyz/flussikatz/searchmovie/MainActivity.kt
@@ -2,10 +2,29 @@ package xyz.flussikatz.searchmovie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun clickSearch (view: View) {
+        Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+    }
+
+    fun clickHistory (view: View) {
+        Toast.makeText(this, "History", Toast.LENGTH_SHORT).show()
+    }
+
+    fun clickSettings (view: View) {
+        Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+    }
+
+    fun clickMarked (view: View) {
+        Toast.makeText(this, "Marked", Toast.LENGTH_SHORT).show()
+    }
+
 }
