a96d6e812e1de3b6f9b5ca6db6efff16ba513f44
flussigkatz <flussigkatz@gmail.com>
2021-05-19 11:25:43 +0300

    Small refactor.

--- app/src/main/java/xyz/flussikatz/searchmovie/MainFragment.kt
+++ app/src/main/java/xyz/flussikatz/searchmovie/MainFragment.kt
@@ -1,6 +1,5 @@
 package xyz.flussikatz.searchmovie
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,8 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
